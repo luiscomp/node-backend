a79fa57cdb4e8182932eb7c1b22db55286196757
luis.ferreira <luis.ferreira@tre-pi.jus.br>
2020-02-12 15:54:30 -0300

    Adicionado gulp para compilação do projeto

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var ts = require('gulp-typescript');
+
+var tsProject = ts.createProject('tsconfig.json');
+
+gulp.task('compile', function() {
+    return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function() {
+    return gulp.src('dist').pipe(clean());
+});
+
+gulp.task('copy-opts', function() {
+    return gulp.src('tests/unit/config/mocha.opts')
+    .pipe(gulp.dest('dist/tests/unit/config'))
+    .pipe(gulp.dest('dist/tests/integration/config'));
+});
+
+gulp.task('default', gulp.series('clean', 'compile', 'copy-opts'));
